33a67747de6c65e10d5a55fbc049b4602c5c6039
Attila Bagossy <bagossyattila@outlook.com>
2019-05-23 19:10:21 +0200

    array-sum/fail-on-empty challenge

---
+++ src/challenges/fault-seeding/array-sum/solutions/fail-on-empty/description.md
@@ -0,0 +1,8 @@
+Initializes an accumulator with the first element thus fails if the argument is an empty array.
+
+## Examples
+
+~~~~JavaScript
+// will throw
+arraySum([]);
+~~~~
---
+++ src/challenges/fault-seeding/array-sum/solutions/fail-on-empty/index.js
@@ -0,0 +1,14 @@
+function arraySum(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    let acc = array[0];
+    for (let i = 1; i < array.length; ++i) {
+        acc += array[i]
+    }
+
+    return acc
+};
+
+module.exports = arraySum;
