a35c1b60050fcb47112315a71e2a9ca27a13cbe2
Attila Bagossy <bagossyattila@outlook.com>
2019-05-23 19:12:09 +0200

    array-sum/miss-last challenge

---
+++ src/challenges/fault-seeding/array-sum/solutions/miss-last/description.md
@@ -0,0 +1,8 @@
+Misses the last element of the array.
+
+## Examples
+
+~~~~JavaScript
+// will return 3
+arraySum([1, 2, 3]);
+~~~~
---
+++ src/challenges/fault-seeding/array-sum/solutions/miss-last/index.js
@@ -0,0 +1,14 @@
+function arraySum(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    let acc = 0;
+    for (let i = 0; i < array.length - 1; ++i) {
+        acc += array[i]
+    }
+
+    return acc
+};
+
+module.exports = arraySum;
