e22998860510e3982f250b7e154692c67f86bc6d
Attila Bagossy <bagossyattila@outlook.com>
2019-05-26 19:18:10 +0200

    Start and end matchers in the email regexps

--- src/challenges/fault-seeding/email-validation/evaluator/index.js
+++ src/challenges/fault-seeding/email-validation/evaluator/index.js
@@ -1,5 +1,5 @@
 function emailValidation(email) {
-    return /[a-zA-Z0-9][a-zA-Z0-9\-\_]*\@[a-zA-Z0-9][a-zA-Z0-9\-\_]*\.[a-zA-Z]+/.test(email);
+    return /^[a-zA-Z0-9][a-zA-Z0-9\-\_]*\@[a-zA-Z0-9][a-zA-Z0-9\-\_]*\.[a-zA-Z]+$/.test(email);
 };
 
 module.exports = emailValidation;
